3f31296bf5db0d05ed5c00d9bc3d1576fd29b13b
nicekenny <nicekenny@163.com>
2019-05-13 18:19:00 +0800

    Update services.js

--- js/services.js
+++ js/services.js
@@ -309,7 +309,7 @@ function doBuy(a) {
 
 	var tpwd_html = "<div class=\"tao_pwd\">"
 		+"<div class=\"tpwd_content\"><p>"+title+"</p><p style=\"color:#0099CC;\">淘口令：&nbsp;"+tpwd+"</p><p>优惠价：&nbsp;"+price+"元</p></div>"
-		+"<div class=\"item_qrcode\" style=\"display:none;\"><img src=\"http://qr.liantu.com/api.php?bg=f3f3f3&fg=ff0000&gc=222222&el=l&w=200&m=10&text="+buyUrl+"\"/></div>"
+		+"<div class=\"item_qrcode\" style=\"display:none;\"><img src=\"http://qr.liantu.com/api.php?bg=f3f3f3&fg=ff0000&gc=222222&el=l&w=120&m=10&text="+buyUrl+"\"/></div>"
 		+"<div class=\"tpwd_info\">复制淘口令，打开"+userType_txt+"APP购买</div>"
 		+"<div class=\"tpwd_links\">"
 		+"<a class=\"tpwd_qrcode\">二维码</a>"
