ab38a57ed197e453ba815173d8189a954ca320c3
nicekenny <nicekenny@163.com>
2019-05-13 16:07:25 +0800

    Update services.js

--- js/services.js
+++ js/services.js
@@ -307,7 +307,7 @@ function doBuy(a) {
 		+"</div></div>";
 
 	var tpwd_dialog = new dialogLayer();
-	var tpwd_dgContent = dialog.open("淘口令",250,300);
+	var tpwd_dgContent = tpwd_dialog.open("淘口令",250,300);
 
 	$(tpwd_dgContent).html(tpwd_html);
 	$(tpwd_dialog.getDialog()).fadeIn(500);
