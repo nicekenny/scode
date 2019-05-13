f2511bc8f4e502f7437dca49271099e60c2789c9
nicekenny <nicekenny@163.com>
2019-05-13 12:02:43 +0800

    Update services.js

--- js/services.js
+++ js/services.js
@@ -57,7 +57,7 @@ $(function() {
 
 			// 固定导航条
 			var category_list = $("#category_list");
-			if(window_top>50) {
+			if(window_top>category_list.offset().top) {
 				if(!category_list.hasClass("nav_fixed")) {
 					category_list.addClass("nav_fixed");
 				}
